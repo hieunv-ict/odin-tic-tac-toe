0d2473342260835485fa7313407a77c0ae35a929
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-23 11:43:35 +0700

    Implementing logic for checking winner

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,26 @@
 let gameBoard = (function (doc) {
+    let size = 3;
     let board = [];
-
-    let cells = doc.querySelectorAll(".cell");
-
+    let boardElem = doc.querySelector(".board");
     function init(){
-        for(let cell of cells){
-            cell.addEventListener("click", e =>{
-                cell.textContent = gameTurn.move();
-            })
+        board = Array.from(Array(3), () => new Array(3));
+        for (let i = 0; i < size; i++){
+            let row = doc.createElement("div");
+            row.classList.add("board-row");
+            for (let j = 0; j < size; j++){
+                let cell = doc.createElement("div");
+                cell.classList.add("cell");
+                cell.addEventListener("click", e => {
+                    if (board[i][j] === undefined){
+                        let val = gameTurn.move();
+                        cell.textContent = val;
+                        board[i][j] = val;
+                        console.log(gameTurn.checkVictory(board));
+                    }
+                })
+                row.appendChild(cell);
+            }
+            boardElem.appendChild(row);
         }
     }
     init();
@@ -22,7 +35,6 @@ let gameTurn = (function () {
 
     let move = function(){
         turn++;
-        console.log(turn);
         if (turn%2===0){
             return player2.symbol;
         }
@@ -30,7 +42,32 @@ let gameTurn = (function () {
             return player1.symbol;
         }
     }
-    return {move};
+    
+    // do the check for 3 adjacent of a symbol in a row/column/diagonal to find winner (val = 3)
+    let checkVictory = function(board){
+        let size = board.length;
+        console.log(size);
+        // 3 adjacent cell on a row is the same -> has winner -> return true
+        // check row
+        for (let i = 0; i < size; i++){
+            for (let j = 0; j < size - 2; j++){
+                let symbol = board[i][j];
+                if (symbol !== undefined){
+                    if (symbol === board[i][j+1] && symbol === board[i][j+2]){
+                        return true;
+                    }
+                }
+            }
+        }
+        // check column
+        for (let i = 0; i < size; i++){
+            for (let j = 0; j < size - 2; j++){
+                let symbol = board[i][j]
+            }
+        }
+        return false;
+    }
+    return {move, checkVictory};
 })();
 
 function createPlayer(symbol){
